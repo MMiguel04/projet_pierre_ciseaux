42cbbb67ccdc3bff1ad740ccb9034e77ab50a309
miguel <migueljafe04@gmail.com>
2024-09-27 12:48:57 +0200

    Correction et declarer le vanquer.
    plusiers modifications, function pour declarer le vanquer et assigner le resultat dans le DOM.
    Ccorrection de faute de frappe

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ let btnCiseaux = document.querySelector("#ciseaux")
 
 function computerChoice(){
     let choices = ["papier", "pierre", "ciseaux"];
-    let randonChoices = Math.floor(Math.random() * choices.length)
+    let randomChoices = Math.floor(Math.random() * choices.length)
 
-    return choices[randonChoices]
+    return choices[randomChoices]
 }
 
 
 function playRound(playerSelection){
-    playerSelection.toLowerCase()
+    
     let computerSelection = computerChoice();
     let divResult = document.querySelector("#result");
 
@@ -24,8 +24,8 @@ function playRound(playerSelection){
         result = "Egalité, vous avez tout les deux choisi" +playerSelection
     } else if (
         (playerSelection === 'pierre' && computerSelection === 'ciseaux') ||
-        (playerSelection === 'feuille' && computerSelection === 'pierre') ||
-        (playerSelection === 'ciseaux' && computerSelection === 'feuille')
+        (playerSelection === 'papier' && computerSelection === 'pierre') ||
+        (playerSelection === 'ciseaux' && computerSelection === 'papier')
     ){
         result = `Vous avez gagné ! ${playerSelection} bat ${computerSelection}.`
         playerScore++
@@ -33,16 +33,32 @@ function playRound(playerSelection){
         result = `Dommage ! ${computerSelection} bat ${playerSelection}`
         computerScore++
     }
+
     divResult.textContent = result
     updateScore()
+    if(playerScore === 5 || computerScore === 5){
+        declareWins()
+    }
 }
 
 function updateScore(){
-    document.querySelector("#player-score").textContent = playerScore;
-    document.querySelector("#ordinateur-score").textContent= computerScore;
+    document.querySelector("#player-score").textContent = `Score du joeur : ${playerScore}`
+    document.querySelector("#ordinateur-score").textContent= `Score de l'ordinateur : ${computerScore}`;
+}
+
+function declareWins(){
+    const divFinalResult = document.querySelector("#final-resultat");
+
+    if(playerScore === 5){
+        divFinalResult.textContent = 'Felicitations ! Vous avez gagne la partie';
+    }else{
+        divFinalResult.textContent = 'Dommage ! ordinateur a gagné la partie'
+    }
 }
 
 playRound("pierre")
 playRound("papier")
 playRound("ciseaux")
+
+
 
