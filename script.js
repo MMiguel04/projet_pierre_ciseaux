c96f17902b85376a4b0c904d7dd124dbffb33d32
miguel <migueljafe04@gmail.com>
2024-09-27 09:37:39 +0200

    Declaration variables. Varibles score du jeu et selection des boutons

---
+++ script.js
@@ -0,0 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let btnPapier = document.querySelector("#papier")
+let btnPierre = document.querySelector("#pierre")
+let btnCiseaux = document.querySelector("#ciseaux")
+
